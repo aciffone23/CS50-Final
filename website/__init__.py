c79d115518edae5836111deba5fa52033106c15d
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-10-12 16:55:18 -0400

    adding spotify api

--- website/__init__.py
+++ website/__init__.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Fantasy Football God'
+    app.config['SECRET_KEY'] = 'CS50'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -19,7 +20,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note 
+    from .models import User
 
     with app.app_context():
         db.create_all()

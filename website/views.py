df5655d5dfdb1bc9566dfb84feb8050531df7ca2
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-11-13 16:31:15 -0500

    fixed home, search, and your library styling and hovering

--- website/views.py
+++ website/views.py
@@ -7,3 +7,9 @@ views = Blueprint('views', __name__)
 # @login_required
 def home():
         return render_template("home.html", user=current_user)
+
+
+@views.route('/search', methods=['GET', 'POST'])
+# @login_required
+def search():
+        return render_template("search.html", user=current_user)
